030515f61323aebbd16cbc8c789eea652857fd7d
Your Name <you@example.com>
2023-03-04 16:20:51 -0500

    update code based on task

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -59,16 +59,18 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
-                    <input
+                        <text style={{ marginLeft: '2rem' }}>£</text>
+                        <input
                         required='required'
                         type='number'
                         id='cost'
                         value={cost}
-                        style={{ marginLeft: '2rem' , size: 10}}
+                        style={{ marginLeft: '0rem' , size: 10}}
                         onChange={(event) => setCost(event.target.value)}>
                         </input>
-
+                    
+                    
+                    
                     <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
                         Save
                     </button>
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,36 @@
 
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+import * as NumericInput from "react-numeric-input";
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, expenses, dispatch } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total = total + item.cost);
+    }, 0);
+    const maxBudget  = 20000;
+    const changeValue = (newValue) => {
+        console.log(newValue)
+        if(newValue > maxBudget) {
+            alert("The value cannot exceed max budget  £"+maxBudget);
+            return;
+        } 
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: newValue,
+        });
+        if (budget < totalExpenses) {
+            alert("You cannot lower budget value than the spending");
+            return;
+        }
+    }
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <text>Budget: £</text>
+            <span>
+            <NumericInput step={10} precision={0} value={budget} onChange={changeValue}/>
+            </span>
         </div>
     );
 };
